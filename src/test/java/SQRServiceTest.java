6cbb5d6267ad0e3049a2a62711e865149e4c1862
DrEvil <opc_senator4@mail.ru>
2022-10-30 22:49:43 +0300

    Поправлены зависимости

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,6 +1,6 @@
-
-
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class SQRServiceTest {
 
